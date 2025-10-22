62746a25919abcf097af91ece220ee80dd099282
magwindow <antosha.petrakov@yandex.ru>
2025-10-22 14:08:08 +0300

    update bot well done

--- bot.py
+++ bot.py
@@ -22,7 +22,21 @@ def menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton('🔍 Поиск собеседника')
     markup.add(item1)
-    bot.send_message(message.chat.id, '📋 Меню', reply_markup=markup)    
+    bot.send_message(message.chat.id, '📋 Меню', reply_markup=markup) 
+    
+
+@bot.message_handler(commands=['stop'])
+def stop(message):
+    chat_info = db.get_active_chat(message.chat.id)  
+    if chat_info:
+        db.delete_chat(chat_info[0])
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('🔍 Поиск собеседника')
+        markup.add(item1)
+        bot.send_message(chat_info[1], 'Собеседник покинул чат', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Вы покинули чат', reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, 'Вы не в чате', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
@@ -40,10 +54,10 @@ def bot_message(message):
                 bot.send_message(message.chat.id, '🔎 Ищем людей....', reply_markup=markup)
             else:
                 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-                item1 = types.KeyboardButton('✋ Завершить диалог')
+                item1 = types.KeyboardButton('/stop')
                 markup.add(item1)
-                bot.send_message(message.chat.id, '📣 Поиск завершен! Собеседник найден.', reply_markup=markup)
-                bot.send_message(chat_two, '📣 Поиск завершен! Собеседник найден.', reply_markup=markup)
+                bot.send_message(message.chat.id, '📣 Поиск завершен! Собеседник найден.\nЧтобы завершить чат нажмите /stop', reply_markup=markup)
+                bot.send_message(chat_two, '📣 Поиск завершен! Собеседник найден.\nЧтобы завершить чат нажмите /stop', reply_markup=markup)
         
         elif message.text == '❌ Отменить поиск':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -52,9 +66,12 @@ def bot_message(message):
             
             db.remove_queue(message.chat.id)
             bot.send_message(message.chat.id, '❌ Поиск остановлен', reply_markup=markup)
+        
+        else:
+            chat_info = db.get_active_chat(message.chat.id)
+            bot.send_message(chat_info[1], message.text)
             
-    
-    
-    
+            
+
     
 bot.polling(none_stop=True)
--- database.py
+++ database.py
@@ -13,6 +13,10 @@ class Database:
         with self.connection:
             self.cursor.execute("DELETE FROM queue WHERE chat_id=?", (chat_id,))
             
+    def delete_chat(self, id_chat):
+        with self.connection:
+            self.cursor.execute("DELETE FROM chats WHERE id=?", (id_chat,))
+            
     def get_chat(self):
         with self.connection:
             chat = self.cursor.execute("SELECT * FROM queue").fetchmany(1)
@@ -28,3 +32,24 @@ class Database:
                 self.cursor.execute("DELETE FROM queue WHERE chat_id=?", (chat_two,))
                 self.cursor.execute("INSERT INTO chats (chat_one, chat_two) VALUES (?, ?)", (chat_one, chat_two))
                 return True
+            
+    def get_active_chat(self, chat_id):
+        with self.connection:
+            chat = self.cursor.execute("SELECT * FROM chats WHERE chat_one=?", (chat_id,))
+            id_chat = 0
+            for row in chat:
+                id_chat = row[0]
+                chat_info = row[0], row[2]
+                
+            if id_chat == 0:
+                chat = self.cursor.execute("SELECT * FROM chats WHERE chat_two=?", (chat_id,))
+                for row in chat:
+                    id_chat = row[0]
+                    chat_info = row[0], row[1]
+            
+                if id_chat == 0:
+                    return False
+                else:
+                    return chat_info
+            else:
+                return chat_info
